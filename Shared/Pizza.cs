5ae40268615a039d0d112f9fbc094c77b768878a
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-08 15:47:06 -0400

    Agregando clases menu, picantes y pizzas.

---
+++ Shared/Pizza.cs
@@ -0,0 +1,17 @@
+﻿namespace PizzaPlace.Shared
+{
+    public class Pizza
+    {
+        public Pizza(int id, string nombre, decimal precio, Picantes picantes)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Precio = precio;
+            this.picante = picantes;
+        }
+        public int Id { get; }
+        public string Nombre { get; }
+        public decimal Precio { get; }
+        public Picantes picante { get; }
+    }
+}
