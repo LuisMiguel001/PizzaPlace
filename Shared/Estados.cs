e80f13e289748db44105ac9b8cb1a7785f7e11f6
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-09 00:11:35 -0400

    Arreglando el menu y actulizando el estado.

--- Shared/Estados.cs
+++ Shared/Estados.cs
@@ -7,5 +7,8 @@
         public CestasDeCompras Cestas { get; } = new CestasDeCompras();
 
         public Interfaces Interfaces { get; set; } = new Interfaces();
-    }
+
+		public decimal PrecioTotal
+        => Cestas.Pedidos.Sum(id => Menu.GetPizza(id)!.Precio);
+	}
 }
