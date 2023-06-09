a81adf1c05b94af5de47628670aef97557610243
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-08 23:56:18 -0400

    Agregando el menu y el metodo eliminar.

--- Shared/CestasDeCompras.cs
+++ Shared/CestasDeCompras.cs
@@ -1,11 +1,17 @@
 ﻿namespace PizzaPlace.Shared
 {
-    public class CestasDeCompras
-    {
-        public Clientes Clientes { get; set; } = new Clientes();
+	public class CestasDeCompras
+	{
+		public Clientes Clientes { get; set; } = new Clientes();
 
-        public List<int> Pedidos { get; set; } = new List<int>();
+		public List<int> Pedidos { get; set; } = new List<int>();
 
-        public bool Pago { get; set; }
-    }
+		public bool Pago { get; set; }
+
+		public void Add(int pizzaId)
+		=> Pedidos.Add(pizzaId);
+
+		public void RemoveAt(int pos)
+		=> Pedidos.RemoveAt(pos);
+	}
 }
