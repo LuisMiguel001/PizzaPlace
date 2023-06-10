99b42b93ac96c241eb0fa014ceeb29c2579fe340
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-06-09 23:51:10 -0400

    Agregando la validacion.

--- Shared/Clientes.cs
+++ Shared/Clientes.cs
@@ -1,13 +1,18 @@
-﻿namespace PizzaPlace.Shared
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaPlace.Shared
 {
     public class Clientes
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Proporcione un nombre")]
         public string Nombre { get; set; } = default!;
 
-        public string Calle { get; set; } = default!;
+		[Required(ErrorMessage = "Proporcione una calle con número de casa")]
+		public string Calle { get; set; } = default!;
 
-        public string Ciudad { get; set; } = default!;
+		[Required(ErrorMessage = "Proporcione una ciudad")]
+		public string Ciudad { get; set; } = default!;
     }
 }
